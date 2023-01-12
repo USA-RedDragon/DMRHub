041bf33a75c863cd56aeb0f71d1448c006257588
Jacob McSwain <jacob@mcswain.dev>
2023-01-12 16:40:56 -0600

    Gorilla Mux is dead :(
    Switch to gin

--- http/api/controllers/v1/meta.go
+++ http/api/controllers/v1/meta.go
@@ -3,16 +3,16 @@ package v1
 import (
 	"fmt"
 	"io"
-	"net/http"
 	"time"
 
 	"github.com/USA-RedDragon/dmrserver-in-a-box/sdk"
+	"github.com/gin-gonic/gin"
 )
 
-func GETVersion(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, fmt.Sprintf("%s-%s", sdk.Version, sdk.GitCommit))
+func GETVersion(c *gin.Context) {
+	io.WriteString(c.Writer, fmt.Sprintf("%s-%s", sdk.Version, sdk.GitCommit))
 }
 
-func GETPing(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, fmt.Sprintf("%d", time.Now().Unix()))
+func GETPing(c *gin.Context) {
+	io.WriteString(c.Writer, fmt.Sprintf("%d", time.Now().Unix()))
 }

ee4224318b054bcaef7619dd1fc7e99bb138e825
Jacob McSwain <jacob@mcswain.dev>
2023-01-13 16:56:10 -0600

    Embed git version again

--- sdk/version.go
+++ sdk/version.go
@@ -1,7 +1,11 @@
 package sdk
 
+import _ "embed"
+
+//go:generate bash -c "../hack/git_commit.sh > commit.txt"
 var (
 	// GitCommit that was compiled. This will be filled in by the compiler.
+	//go:embed commit.txt
 	GitCommit string
 
 	// Version of the program

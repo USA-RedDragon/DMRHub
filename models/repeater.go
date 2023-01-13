a24722ea035eda35334e2d93c0ac3ae24323611d
Jacob McSwain <jacob@mcswain.dev>
2023-01-12 20:31:13 -0600

    Add repeater password

--- models/repeater.go
+++ models/repeater.go
@@ -11,61 +11,67 @@ import (
 
 //go:generate msgp
 type Repeater struct {
-	RadioID       uint           `msg:"radio_id" gorm:"primaryKey"`
-	Connection    string         `msg:"connection"`
-	Connected     time.Time      `msg:"connected"`
-	PingsReceived int            `msg:"pings_received"`
-	LastPing      time.Time      `msg:"last_ping"`
-	IP            string         `msg:"ip"`
-	Port          int            `msg:"port"`
-	Salt          uint32         `msg:"salt"`
-	Callsign      string         `msg:"callsign"`
-	RXFrequency   int            `msg:"rx_frequency"`
-	TXFrequency   int            `msg:"tx_frequency"`
-	TXPower       int            `msg:"tx_power"`
-	ColorCode     int            `msg:"color_code"`
-	Latitude      float32        `msg:"latitude"`
-	Longitude     float32        `msg:"longitude"`
-	Height        int            `msg:"height"`
-	Location      string         `msg:"location"`
-	Description   string         `msg:"description"`
-	Slots         int            `msg:"slots"`
-	URL           string         `msg:"url"`
-	SoftwareID    string         `msg:"software_id"`
-	PackageID     string         `msg:"package_id"`
-	TS1Talkgroups []Talkgroup    `msg:"-" gorm:"many2many:repeater_ts1_talkgroups;"`
-	TS2Talkgroups []Talkgroup    `msg:"-" gorm:"many2many:repeater_ts2_talkgroups;"`
-	CreatedAt     time.Time      `msg:"-" json:"-"`
-	UpdatedAt     time.Time      `msg:"-" json:"-"`
-	DeletedAt     gorm.DeletedAt `gorm:"index" msg:"-" json:"-"`
+	RadioID                uint           `msg:"radio_id" gorm:"primaryKey"`
+	Connection             string         `msg:"connection"`
+	Connected              time.Time      `msg:"connected"`
+	PingsReceived          int            `msg:"pings_received"`
+	LastPing               time.Time      `msg:"last_ping"`
+	IP                     string         `msg:"ip"`
+	Port                   int            `msg:"port"`
+	Salt                   uint32         `msg:"salt"`
+	Callsign               string         `msg:"callsign"`
+	RXFrequency            int            `msg:"rx_frequency"`
+	TXFrequency            int            `msg:"tx_frequency"`
+	TXPower                int            `msg:"tx_power"`
+	ColorCode              int            `msg:"color_code"`
+	Latitude               float32        `msg:"latitude"`
+	Longitude              float32        `msg:"longitude"`
+	Height                 int            `msg:"height"`
+	Location               string         `msg:"location"`
+	Description            string         `msg:"description"`
+	Slots                  int            `msg:"slots"`
+	URL                    string         `msg:"url"`
+	SoftwareID             string         `msg:"software_id"`
+	PackageID              string         `msg:"package_id"`
+	Password               string         `msg:"-" json:"-"`
+	TS1StaticTalkgroups    []Talkgroup    `msg:"-" gorm:"many2many:repeater_ts1_static_talkgroups;"`
+	TS2StaticTalkgroups    []Talkgroup    `msg:"-" gorm:"many2many:repeater_ts2_static_talkgroups;"`
+	TS1TemporaryTalkgroups []Talkgroup    `msg:"-" gorm:"many2many:repeater_ts1_temporary_talkgroups;"`
+	TS2TemporaryTalkgroups []Talkgroup    `msg:"-" gorm:"many2many:repeater_ts2_temporary_talkgroups;"`
+	CreatedAt              time.Time      `msg:"-" json:"-"`
+	UpdatedAt              time.Time      `msg:"-" json:"-"`
+	DeletedAt              gorm.DeletedAt `gorm:"index" msg:"-" json:"-"`
 }
 
 func MakeRepeater(radioId uint, salt uint32, socketAddr net.UDPAddr) Repeater {
 	return Repeater{
-		Connection:    "DISCONNECTED",
-		Connected:     time.UnixMilli(0),
-		PingsReceived: 0,
-		LastPing:      time.UnixMilli(0),
-		IP:            socketAddr.IP.String(),
-		Port:          socketAddr.Port,
-		Salt:          salt,
-		RadioID:       radioId,
-		Callsign:      "",
-		RXFrequency:   0,
-		TXFrequency:   0,
-		TXPower:       0,
-		ColorCode:     0,
-		Latitude:      0,
-		Longitude:     0,
-		Height:        0,
-		Location:      "",
-		Description:   "",
-		Slots:         0,
-		URL:           "",
-		SoftwareID:    "",
-		PackageID:     "",
-		TS1Talkgroups: []Talkgroup{},
-		TS2Talkgroups: []Talkgroup{},
+		Connection:             "DISCONNECTED",
+		Connected:              time.UnixMilli(0),
+		PingsReceived:          0,
+		LastPing:               time.UnixMilli(0),
+		IP:                     socketAddr.IP.String(),
+		Port:                   socketAddr.Port,
+		Salt:                   salt,
+		RadioID:                radioId,
+		Callsign:               "",
+		RXFrequency:            0,
+		TXFrequency:            0,
+		TXPower:                0,
+		ColorCode:              0,
+		Latitude:               0,
+		Longitude:              0,
+		Height:                 0,
+		Location:               "",
+		Description:            "",
+		Slots:                  0,
+		URL:                    "",
+		SoftwareID:             "",
+		PackageID:              "",
+		TS1StaticTalkgroups:    []Talkgroup{},
+		TS2StaticTalkgroups:    []Talkgroup{},
+		TS1TemporaryTalkgroups: []Talkgroup{},
+		TS2TemporaryTalkgroups: []Talkgroup{},
+		Password:               "",
 	}
 }
 

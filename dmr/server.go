64fe153723e4478da2af8f0a8c33c14e297b43ea
Jacob McSwain <jacob@mcswain.dev>
2023-01-14 07:19:07 -0600

    Fix unlink

--- dmr/server.go
+++ dmr/server.go
@@ -267,10 +267,12 @@ func (s DMRServer) handlePacket(remoteAddr *net.UDPAddr, data []byte) {
 			if packet.Dst == 4000 {
 				if packet.Slot {
 					klog.Infof("Unlinking timeslot 2 from %d", packet.Repeater)
-					dbRepeater.TS2DynamicTalkgroupID = 0
+					s.DB.Model(&dbRepeater).Select("TS2DynamicTalkgroupID").Updates(map[string]interface{}{"TS2DynamicTalkgroupID": nil})
+					s.DB.Model(&dbRepeater).Association("TS2DynamicTalkgroup").Delete(&dbRepeater.TS2DynamicTalkgroup)
 				} else {
 					klog.Infof("Unlinking timeslot 1 from %d", packet.Repeater)
-					dbRepeater.TS1DynamicTalkgroupID = 0
+					s.DB.Model(&dbRepeater).Select("TS1DynamicTalkgroupID").Updates(map[string]interface{}{"TS1DynamicTalkgroupID": nil})
+					s.DB.Model(&dbRepeater).Association("TS1DynamicTalkgroup").Delete(&dbRepeater.TS1DynamicTalkgroup)
 				}
 				s.DB.Save(&dbRepeater)
 				return

f7148997154bb2ed1f443cd34c5957e7ccebb95f
Jacob McSwain <jacob@mcswain.dev>
2023-01-17 23:44:57 -0600

    Add API for lastheard

--- http/api/controllers/v1/lastheard/lastheard.go
+++ http/api/controllers/v1/lastheard/lastheard.go
@@ -1,15 +1,62 @@
 package lastheard
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/USA-RedDragon/dmrserver-in-a-box/models"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
 
 func GETLastheard(c *gin.Context) {
+	db := c.MustGet("DB").(*gorm.DB)
+	session := sessions.Default(c)
+	userId := session.Get("user_id").(uint)
+	var calls []models.Call
+	if userId == 0 {
+		// This is okay, we just query the latest X calls
+		calls = models.FindCalls(db, 10)
+	} else {
+		// Get the last calls for the user
+		calls = models.FindUserCalls(db, userId, 10)
+	}
+	c.JSON(http.StatusOK, calls)
 }
 
 func GETLastheardUser(c *gin.Context) {
+	db := c.MustGet("DB").(*gorm.DB)
+	id := c.Param("id")
+	userID64, err := strconv.ParseUint(id, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid User ID"})
+		return
+	}
+	userID := uint(userID64)
+	c.JSON(http.StatusOK, models.FindUserCalls(db, userID, 10))
 }
 
 func GETLastheardRepeater(c *gin.Context) {
+	db := c.MustGet("DB").(*gorm.DB)
+	id := c.Param("id")
+	repeaterID64, err := strconv.ParseUint(id, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Repeater ID"})
+		return
+	}
+	repeaterID := uint(repeaterID64)
+	c.JSON(http.StatusOK, models.FindRepeaterCalls(db, repeaterID, 10))
 }
 
 func GETLastheardTalkgroup(c *gin.Context) {
+	db := c.MustGet("DB").(*gorm.DB)
+	id := c.Param("id")
+	talkgroupID64, err := strconv.ParseUint(id, 10, 32)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid Talkgroup ID"})
+		return
+	}
+	talkgroupID := uint(talkgroupID64)
+	c.JSON(http.StatusOK, models.FindTalkgroupCalls(db, talkgroupID, 10))
 }
--- models/call.go
+++ models/call.go
@@ -39,6 +39,66 @@ type Call struct {
 	DeletedAt      gorm.DeletedAt `json:"-" gorm:"index" msg:"-"`
 }
 
+func FindCalls(db *gorm.DB, limit int) []Call {
+	var calls []Call
+	db.Preload("User").Preload("Repeater").Preload("ToTalkgroup").Preload("ToUser").Preload("ToRepeater").Where("is_to_talkgroup = ?", true).Order("start_time desc").Limit(limit).Find(&calls)
+	return calls
+}
+
+func FindRepeaterCalls(db *gorm.DB, repeaterID uint, limit int) []Call {
+	var toRepeaterCalls []Call
+	var fromRepeaterCalls []Call
+	// Find calls where (IsToRepeater is true and ToRepeaterID is repeaterID) or (RepeaterID is repeaterID)
+	db.Preload("User").Preload("Repeater").Preload("ToRepeater").Where("is_to_repeater = ? AND to_repeater_id = ?", true, repeaterID).Order("start_time desc").Limit(limit).Find(&toRepeaterCalls)
+	db.Preload("User").Preload("Repeater").Preload("ToRepeater").Where("repeater_id = ?", repeaterID).Order("start_time desc").Limit(limit).Find(&fromRepeaterCalls)
+	// Merge the two slices in order of start time
+	var i, j int
+	var mergedCalls []Call
+	for i < len(toRepeaterCalls) && j < len(fromRepeaterCalls) {
+		if toRepeaterCalls[i].StartTime.After(fromRepeaterCalls[j].StartTime) {
+			mergedCalls = append(mergedCalls, toRepeaterCalls[i])
+			i++
+		} else {
+			mergedCalls = append(mergedCalls, fromRepeaterCalls[j])
+			j++
+		}
+	}
+	mergedCalls = append(mergedCalls, toRepeaterCalls[i:]...)
+	mergedCalls = append(mergedCalls, fromRepeaterCalls[j:]...)
+	return mergedCalls
+}
+
+func FindUserCalls(db *gorm.DB, userID uint, limit int) []Call {
+	var toUserCalls []Call
+	var fromUserCalls []Call
+	// Find calls where (IsToUser is true and ToUserID is userID) or (UserID is userID)
+	db.Preload("User").Preload("Repeater").Preload("ToUser").Where("is_to_user = ? AND to_user_id = ?", true, userID).Order("start_time desc").Limit(limit).Find(&toUserCalls)
+	db.Preload("User").Preload("Repeater").Preload("ToUser").Where("user_id = ?", userID).Order("start_time desc").Limit(limit).Find(&fromUserCalls)
+	// Merge the two slices in order of start time
+	var i, j int
+	var mergedCalls []Call
+	for i < len(toUserCalls) && j < len(fromUserCalls) {
+		if toUserCalls[i].StartTime.After(fromUserCalls[j].StartTime) {
+			mergedCalls = append(mergedCalls, toUserCalls[i])
+			i++
+		} else {
+			mergedCalls = append(mergedCalls, fromUserCalls[j])
+			j++
+		}
+	}
+	mergedCalls = append(mergedCalls, toUserCalls[i:]...)
+	mergedCalls = append(mergedCalls, fromUserCalls[j:]...)
+
+	return mergedCalls
+}
+
+func FindTalkgroupCalls(db *gorm.DB, talkgroupID uint, limit int) []Call {
+	var calls []Call
+	// Find calls where (IsToTalkgroup is true and ToTalkgroupID is talkgroupID)
+	db.Preload("User").Preload("Repeater").Preload("ToTalkgroup").Where("is_to_talkgroup = ? AND to_talkgroup_id = ?", true, talkgroupID).Order("start_time desc").Limit(limit).Find(&calls)
+	return calls
+}
+
 func FindActiveCall(db *gorm.DB, streamID uint, src uint, dst uint, slot bool, groupCall bool) (Call, error) {
 	var call Call
 	db.Preload("User").Preload("Repeater").Preload("ToTalkgroup").Preload("ToUser").Preload("ToRepeater").Where("stream_id = ? AND active = ? AND user_id = ? AND destination_id = ? AND time_slot = ? AND group_call = ?", streamID, true, src, dst, slot, groupCall).First(&call)

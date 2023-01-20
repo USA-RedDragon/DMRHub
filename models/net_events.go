d1a18e71d77dbf1d6e647e66d660821923b5677a
Jacob McSwain <jacob@mcswain.dev>
2023-01-20 05:38:37 -0600

    models: Rename NetCheckins -> NetEvent

--- models/net_events.go
+++ models/net_events.go
@@ -6,14 +6,13 @@ import (
 	"gorm.io/gorm"
 )
 
-// Has one net. Contains many calls from who checked in. Contains start and end date and time.
-type NetCheckins struct {
+type NetEvent struct {
 	ID        uint           `json:"-" gorm:"primarykey"`
 	NetID     uint           `json:"-"`
 	Net       Net            `json:"talkgroup" gorm:"foreignKey:NetID"`
 	StartTime time.Time      `json:"start_time"`
 	Duration  time.Duration  `json:"duration"`
-	Checkins  []Call         `json:"checkins" gorm:"many2many:net_checkin_calls;"`
+	Checkins  []Call         `json:"checkins" gorm:"many2many:net_event_checkins;"`
 	CreatedAt time.Time      `json:"-"`
 	UpdatedAt time.Time      `json:"-"`
 	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`

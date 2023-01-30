5ce73927809473269f0ec2a1aabe2e49fee88918
Jacob McSwain <jacob@mcswain.dev>
2023-01-30 13:07:07 -0600

    Try to fix CI git commit

--- hack/git_commit.sh
+++ hack/git_commit.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+git fetch origin main:refs/remotes/origin/main --quiet
 echo -n $(git rev-parse --short HEAD)
 # Check if git is dirty because it differs from origin/main
 # If it differs, echo -dirty

aa246ff5fd3830ff51859d4e05e4a4b2ba7e7b16
Jacob McSwain <jacob@mcswain.dev>
2023-02-02 16:15:41 -0600

    http: middleware: always use a context with DB

--- internal/http/api/middleware/database.go
+++ internal/http/api/middleware/database.go
@@ -1,18 +1,13 @@
 package middleware
 
 import (
-	"github.com/USA-RedDragon/DMRHub/internal/config"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
 func DatabaseProvider(db *gorm.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		if config.GetConfig().OTLPEndpoint != "" {
-			c.Set("DB", db.WithContext(c.Request.Context()))
-		} else {
-			c.Set("DB", db)
-		}
+		c.Set("DB", db.WithContext(c.Request.Context()))
 		c.Next()
 	}
 }

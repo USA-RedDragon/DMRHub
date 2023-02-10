9ce50e135324a8ad8dda724e5104a868a25f784d
Jacob McSwain <jacob@mcswain.dev>
2023-02-10 14:04:32 -0600

    ci: frontend: generate coverage reports

--- internal/http/frontend/cypress.config.js
+++ internal/http/frontend/cypress.config.js
@@ -2,6 +2,14 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    setupNodeEvents(on, config) {
+      require("@cypress/code-coverage/task")(on, config);
+      // include any other plugin code...
+
+      // It's IMPORTANT to return the config object
+      // with any changed environment variables
+      return config;
+    },
     specPattern: "tests/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
     baseUrl: "http://localhost:4173",
   },
--- internal/http/frontend/vite.config.js
+++ internal/http/frontend/vite.config.js
@@ -2,10 +2,19 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import istanbul from "vite-plugin-istanbul";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    istanbul({
+      include: "src/*",
+      exclude: ["node_modules"],
+      requireEnv: false,
+      cypress: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
@@ -13,8 +22,12 @@ export default defineConfig({
   },
   test: {
     coverage: {
+      all: true,
+      enabled: true,
       provider: "c8",
       reporter: ["html", "lcov"],
+      reportsDirectory: "coverage/unit",
+      src: ["src"],
     },
   },
 });

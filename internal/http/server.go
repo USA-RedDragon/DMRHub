7cb0d6a3d205374f8d114ce8aa0c2b64bda1f7c8
Jacob McSwain <jacob@mcswain.dev>
2023-02-13 13:59:56 -0600

    http server: reduce complexity

--- internal/http/server.go
+++ internal/http/server.go
@@ -40,25 +40,51 @@ const rateLimitLimit = 10
 //go:embed frontend/dist/*
 var FS embed.FS
 
+func addMiddleware(r *gin.Engine, db *gorm.DB, redisClient *redis.Client) {
+	// Debug
+	if config.GetConfig().Debug {
+		pprof.Register(r)
+	}
+
+	// Tracing
+	if config.GetConfig().OTLPEndpoint != "" {
+		r.Use(otelgin.Middleware("api"))
+		r.Use(middleware.TracingProvider())
+	}
+
+	// DBs
+	r.Use(middleware.DatabaseProvider(db))
+	r.Use(middleware.PaginatedDatabaseProvider(db, middleware.PaginationConfig{}))
+	r.Use(middleware.RedisProvider(redisClient))
+
+	// CORS
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowCredentials = true
+	corsConfig.AllowOrigins = config.GetConfig().CORSHosts
+	r.Use(cors.New(corsConfig))
+
+	// Sessions
+	sessionStore, _ := redisSessions.NewStore(redisClient, []byte(""), config.GetConfig().Secret)
+	r.Use(sessions.Sessions("sessions", sessionStore))
+}
+
 func CreateRouter(db *gorm.DB, redisClient *redis.Client) *gin.Engine {
 	r := gin.Default()
 
-	ws := websocketHandler.CreateHandler(db, redisClient)
+	if config.GetConfig().Debug {
+		gin.SetMode(gin.DebugMode)
+	} else {
+		gin.SetMode(gin.ReleaseMode)
+	}
 
 	err := r.SetTrustedProxies(config.GetConfig().TrustedProxies)
 	if err != nil {
 		klog.Error(err)
 	}
 
-	if config.GetConfig().Debug {
-		pprof.Register(r)
-		gin.SetMode(gin.DebugMode)
-	} else {
-		gin.SetMode(gin.ReleaseMode)
-	}
-	r.Use(middleware.DatabaseProvider(db))
-	r.Use(middleware.PaginatedDatabaseProvider(db, middleware.PaginationConfig{}))
-	r.Use(middleware.RedisProvider(redisClient))
+	ws := websocketHandler.CreateHandler(db, redisClient)
+
+	addMiddleware(r, db, redisClient)
 
 	ratelimitStore := ratelimit.RedisStore(&ratelimit.RedisOptions{
 		RedisClient: redisClient,
@@ -74,34 +100,23 @@ func CreateRouter(db *gorm.DB, redisClient *redis.Client) *gin.Engine {
 		},
 	})
 
-	corsConfig := cors.DefaultConfig()
-	corsConfig.AllowCredentials = true
-	corsConfig.AllowOrigins = config.GetConfig().CORSHosts
-	r.Use(cors.New(corsConfig))
-
-	sessionStore, _ := redisSessions.NewStore(redisClient, []byte(""), config.GetConfig().Secret)
-	r.Use(sessions.Sessions("sessions", sessionStore))
-
 	ws.ApplyRoutes(r, ratelimitMW)
-
-	r.Use(otelgin.Middleware("api"))
-	r.Use(middleware.TracingProvider())
-
 	api.ApplyRoutes(r, ratelimitMW)
 
-	staticGroup := r.Group("/")
+	addFrontendRoutes(r)
 
-	files, err := getAllFilenames(&FS, "frontend/dist")
-	if err != nil {
-		klog.Errorf("Failed to read directory: %s", err)
-	}
+	return r
+}
+
+func addFrontendWildcards(staticGroup *gin.RouterGroup, depth int) {
 	staticGroup.GET("/", func(c *gin.Context) {
-		file, getErr := FS.Open("frontend/dist/index.html")
-		if getErr != nil {
-			klog.Errorf("Failed to open file: %s", getErr)
+		file, err := FS.Open("frontend/dist/index.html")
+		if err != nil {
+			klog.Errorf("Failed to open file: %s", err)
+			return
 		}
 		defer func() {
-			err = file.Close()
+			err := file.Close()
 			if err != nil {
 				klog.Errorf("Failed to close file: %s", err)
 			}
@@ -112,102 +127,72 @@ func CreateRouter(db *gorm.DB, redisClient *redis.Client) *gin.Engine {
 		}
 		c.Data(http.StatusOK, "text/html", fileContent)
 	})
-	staticGroup.GET("/:wild", func(c *gin.Context) {
-		wild, have := c.Params.Get("wild")
-		if !have {
-			klog.Errorf("Failed to get wildcard: %s", err)
-			return
+	wildcard := "/:wild"
+	for i := 0; i < depth; i++ {
+		// We need to make a string that contains /:wild for each depth
+		// After the first depth, we need to add a number to the end of the wild
+		// Example for depth 3: /:wild/:wild2/:wild3
+		if i > 0 {
+			wildcard += fmt.Sprintf("/:wild%d", i)
 		}
-		file, fileErr := FS.Open(path.Join("frontend/dist", wild))
-		if fileErr != nil {
-			file, fileErr = FS.Open("frontend/dist/index.html")
+		thisDepth := i
+		staticGroup.GET(wildcard, func(c *gin.Context) {
+			wildPath := "frontend/dist"
+			if thisDepth > 0 {
+				// We need to get the wildcards and add them to the path
+				// Example for depth 3: /:wild/:wild2/:wild3
+
+				// Get the first wildcard
+				wild, have := c.Params.Get("wild")
+				if !have {
+					klog.Errorf("Failed to get wildcard")
+					return
+				}
+				// Add the first wildcard to the path
+				wildPath = path.Join(wildPath, wild)
+				// Get the rest of the wildcards
+				for j := 1; j <= thisDepth; j++ {
+					wild, have := c.Params.Get(fmt.Sprintf("wild%d", j))
+					if !have {
+						klog.Errorf("Failed to get wildcard")
+						return
+					}
+					wildPath = path.Join(wildPath, wild)
+				}
+			}
+			file, fileErr := FS.Open(wildPath)
 			if fileErr != nil {
-				klog.Errorf("Failed to open file: %s", fileErr)
+				file, fileErr = FS.Open("frontend/dist/index.html")
+				if fileErr != nil {
+					klog.Errorf("Failed to open file: %s", fileErr)
+					return
+				}
+			}
+			defer func() {
+				err := file.Close()
+				if err != nil {
+					klog.Errorf("Failed to close file: %s", err)
+				}
+			}()
+			fileContent, readErr := io.ReadAll(file)
+			if readErr != nil {
+				klog.Errorf("Failed to read file: %s", readErr)
 				return
 			}
-		}
-		defer func() {
-			err = file.Close()
-			if err != nil {
-				klog.Errorf("Failed to close file: %s", err)
-			}
-		}()
-		fileContent, readErr := io.ReadAll(file)
-		if readErr != nil {
-			klog.Errorf("Failed to read file: %s", readErr)
-			return
-		}
-		c.Data(http.StatusOK, "text/html", fileContent)
-	})
-	staticGroup.GET("/:wild/:wild2", func(c *gin.Context) {
-		wild, have := c.Params.Get("wild")
-		if !have {
-			klog.Errorf("Failed to get wildcard: %s", err)
-			return
-		}
-		wild2, have := c.Params.Get("wild2")
-		if !have {
-			klog.Errorf("Failed to get wildcard: %s", err)
-			return
-		}
-		file, insideErr := FS.Open(path.Join("frontend/dist", wild, wild2))
-		if insideErr != nil {
-			file, insideErr = FS.Open("frontend/dist/index.html")
-			if insideErr != nil {
-				klog.Errorf("Failed to open file: %s", err)
-				return
-			}
-		}
-		defer func() {
-			err = file.Close()
-			if err != nil {
-				klog.Errorf("Failed to close file: %s", err)
-			}
-		}()
-		fileContent, nextErr := io.ReadAll(file)
-		if nextErr != nil {
-			klog.Errorf("Failed to read file: %s", nextErr)
-			return
-		}
-		c.Data(http.StatusOK, "text/html", fileContent)
-	})
-	staticGroup.GET("/:wild/:wild2/:wild3", func(c *gin.Context) {
-		wild, have := c.Params.Get("wild")
-		if !have {
-			klog.Errorf("Failed to get wildcard: %s", err)
-			return
-		}
-		wild2, have := c.Params.Get("wild2")
-		if !have {
-			klog.Errorf("Failed to get wildcard: %s", err)
-			return
-		}
-		wild3, have := c.Params.Get("wild3")
-		if !have {
-			klog.Errorf("Failed to get wildcard: %s", err)
-			return
-		}
-		file, fileErr := FS.Open(path.Join("frontend/dist", wild, wild2, wild3))
-		if fileErr != nil {
-			file, fileErr = FS.Open("frontend/dist/index.html")
-			if fileErr != nil {
-				klog.Errorf("Failed to open file: %s", fileErr)
-				return
-			}
-		}
-		defer func() {
-			err = file.Close()
-			if err != nil {
-				klog.Errorf("Failed to close file: %s", err)
-			}
-		}()
-		fileContent, fileErr := io.ReadAll(file)
-		if fileErr != nil {
-			klog.Errorf("Failed to read file: %s", fileErr)
-			return
-		}
-		c.Data(http.StatusOK, "text/html", fileContent)
-	})
+			c.Data(http.StatusOK, "text/html", fileContent)
+		})
+	}
+}
+
+func addFrontendRoutes(r *gin.Engine) {
+	staticGroup := r.Group("/")
+
+	files, err := getAllFilenames(&FS, "frontend/dist")
+	if err != nil {
+		klog.Errorf("Failed to read directory: %s", err)
+	}
+	const wildcardDepth = 4
+	addFrontendWildcards(staticGroup, wildcardDepth)
 	for _, entry := range files {
 		staticName := strings.Replace(entry, "frontend/dist", "", 1)
 		if staticName == "" {
@@ -231,53 +216,56 @@ func CreateRouter(db *gorm.DB, redisClient *redis.Client) *gin.Engine {
 				klog.Errorf("Failed to read file: %s", fileErr)
 				return
 			}
-			switch {
-			case strings.HasSuffix(c.Request.URL.Path, ".js"):
-				c.Data(http.StatusOK, "text/javascript", fileContent)
-				return
-			case strings.HasSuffix(c.Request.URL.Path, ".css"):
-				c.Data(http.StatusOK, "text/css", fileContent)
-				return
-			case strings.HasSuffix(c.Request.URL.Path, ".html") || strings.HasSuffix(entry, ".htm"):
-				c.Data(http.StatusOK, "text/html", fileContent)
-				return
-			case strings.HasSuffix(c.Request.URL.Path, ".ico"):
-				c.Data(http.StatusOK, "image/x-icon", fileContent)
-				return
-			case strings.HasSuffix(c.Request.URL.Path, ".png"):
-				c.Data(http.StatusOK, "image/png", fileContent)
-				return
-			case strings.HasSuffix(c.Request.URL.Path, ".jpg") || strings.HasSuffix(entry, ".jpeg"):
-				c.Data(http.StatusOK, "image/jpeg", fileContent)
-				return
-			case strings.HasSuffix(c.Request.URL.Path, ".webp"):
-				c.Data(http.StatusOK, "image/webp", fileContent)
-				return
-			case strings.HasSuffix(c.Request.URL.Path, ".svg"):
-				c.Data(http.StatusOK, "image/svg+xml", fileContent)
-				return
-			case strings.HasSuffix(c.Request.URL.Path, ".gif"):
-				c.Data(http.StatusOK, "image/gif", fileContent)
-				return
-			case strings.HasSuffix(c.Request.URL.Path, ".json"):
-				c.Data(http.StatusOK, "application/json", fileContent)
-				return
-			case strings.HasSuffix(c.Request.URL.Path, ".xml"):
-				c.Data(http.StatusOK, "text/xml", fileContent)
-				return
-			case strings.HasSuffix(c.Request.URL.Path, ".txt"):
-				c.Data(http.StatusOK, "text/plain", fileContent)
-				return
-			case strings.HasSuffix(c.Request.URL.Path, ".webmanifest"):
-				c.Data(http.StatusOK, "application/manifest+json", fileContent)
-				return
-			default:
-				c.Data(http.StatusOK, "text/plain", fileContent)
-				return
-			}
+			handleMime(c, fileContent, entry)
 		})
 	}
-	return r
+}
+
+func handleMime(c *gin.Context, fileContent []byte, entry string) {
+	switch {
+	case strings.HasSuffix(c.Request.URL.Path, ".js"):
+		c.Data(http.StatusOK, "text/javascript", fileContent)
+		return
+	case strings.HasSuffix(c.Request.URL.Path, ".css"):
+		c.Data(http.StatusOK, "text/css", fileContent)
+		return
+	case strings.HasSuffix(c.Request.URL.Path, ".html") || strings.HasSuffix(entry, ".htm"):
+		c.Data(http.StatusOK, "text/html", fileContent)
+		return
+	case strings.HasSuffix(c.Request.URL.Path, ".ico"):
+		c.Data(http.StatusOK, "image/x-icon", fileContent)
+		return
+	case strings.HasSuffix(c.Request.URL.Path, ".png"):
+		c.Data(http.StatusOK, "image/png", fileContent)
+		return
+	case strings.HasSuffix(c.Request.URL.Path, ".jpg") || strings.HasSuffix(entry, ".jpeg"):
+		c.Data(http.StatusOK, "image/jpeg", fileContent)
+		return
+	case strings.HasSuffix(c.Request.URL.Path, ".webp"):
+		c.Data(http.StatusOK, "image/webp", fileContent)
+		return
+	case strings.HasSuffix(c.Request.URL.Path, ".svg"):
+		c.Data(http.StatusOK, "image/svg+xml", fileContent)
+		return
+	case strings.HasSuffix(c.Request.URL.Path, ".gif"):
+		c.Data(http.StatusOK, "image/gif", fileContent)
+		return
+	case strings.HasSuffix(c.Request.URL.Path, ".json"):
+		c.Data(http.StatusOK, "application/json", fileContent)
+		return
+	case strings.HasSuffix(c.Request.URL.Path, ".xml"):
+		c.Data(http.StatusOK, "text/xml", fileContent)
+		return
+	case strings.HasSuffix(c.Request.URL.Path, ".txt"):
+		c.Data(http.StatusOK, "text/plain", fileContent)
+		return
+	case strings.HasSuffix(c.Request.URL.Path, ".webmanifest"):
+		c.Data(http.StatusOK, "application/manifest+json", fileContent)
+		return
+	default:
+		c.Data(http.StatusOK, "text/plain", fileContent)
+		return
+	}
 }
 
 // Start the HTTP server.

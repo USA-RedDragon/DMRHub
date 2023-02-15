4cb1d90731df78262b96b6c30236dfb614631f7b
Jacob McSwain <jacob@mcswain.dev>
2023-02-15 23:47:53 +0000

    Cypress: Generate screenshots

--- internal/http/frontend/cypress/support/e2e.js
+++ internal/http/frontend/cypress/support/e2e.js
@@ -13,7 +13,10 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 import "@cypress/code-coverage/support";
-import 'cypress-mochawesome-reporter/register';
+import "cypress-mochawesome-reporter/register";
+import { registerCommand } from "cypress-wait-for-stable-dom";
+
+registerCommand();
 
 // Import commands.js using ES2015 syntax:
 import "./commands";

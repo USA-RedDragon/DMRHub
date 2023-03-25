c84580e361aa0334339fef86708c83cf73f9b122
Jacob McSwain <jacob@mcswain.dev>
2023-03-25 00:13:54 -0500

    Fix copyright header lint

--- internal/http/sessions/redis.go
+++ internal/http/sessions/redis.go
@@ -1,3 +1,4 @@
+//nolint:golint,goheader
 // Copyright (c) 2016 Gin-Gonic. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
--- internal/http/sessions/redistore.go
+++ internal/http/sessions/redistore.go
@@ -1,3 +1,4 @@
+//nolint:golint,goheader
 // Copyright 2012 Brian "bojo" Jones. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.

249415b01dffa76f6e85d6e364909fdfed4512ca
Jacob McSwain <jacob@mcswain.dev>
2023-04-25 18:36:59 -0500

    Back to c8 coverage

--- internal/http/frontend/vite.config.js
+++ internal/http/frontend/vite.config.js
@@ -2,20 +2,11 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import istanbul from 'vite-plugin-istanbul';
-import process from 'process';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    istanbul({
-      include: 'src/*',
-      exclude: ['node_modules'],
-      requireEnv: false,
-      cypress: true,
-      forceBuildInstrument: process.env.NODE_ENV === 'test',
-    }),
   ],
   resolve: {
     alias: {
@@ -31,7 +22,7 @@ export default defineConfig({
     coverage: {
       all: true,
       enabled: true,
-      provider: 'istanbul',
+      provider: 'c8',
       reporter: ['html', 'lcov', 'text'],
       reportsDirectory: 'coverage/unit',
       src: ['src'],

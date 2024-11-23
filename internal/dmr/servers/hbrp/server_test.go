974daa9f59f0f2de38f1b461d4c958ebeb647995
Jacob McSwain <jacob@mcswain.dev>
2024-11-23 12:24:20 -0600

    servers: hbrp: test initialization

--- internal/dmr/servers/hbrp/server_test.go
+++ internal/dmr/servers/hbrp/server_test.go
@@ -21,9 +21,38 @@ package hbrp_test
 
 import (
 	"testing"
+
+	"github.com/USA-RedDragon/DMRHub/internal/dmr/calltracker"
+	"github.com/USA-RedDragon/DMRHub/internal/dmr/servers"
+	"github.com/USA-RedDragon/DMRHub/internal/dmr/servers/hbrp"
+	"gorm.io/gorm"
 )
 
-func TestNoop(t *testing.T) {
-	t.Parallel()
-	t.Log("Noop")
+func TestMakeServerInitialization(t *testing.T) {
+	db := &gorm.DB{}
+	redisClient := &servers.RedisClient{}
+	callTracker := &calltracker.CallTracker{}
+	version := "1.0.0"
+	commit := "abc123"
+
+	server := hbrp.MakeServer(db, nil, redisClient, callTracker, version, commit)
+
+	if server.DB != db {
+		t.Errorf("Expected DB to be %v, got %v", db, server.DB)
+	}
+	if server.Redis != redisClient {
+		t.Errorf("Expected Redis to be %v, got %v", redisClient, server.Redis)
+	}
+	if server.CallTracker != callTracker {
+		t.Errorf("Expected CallTracker to be %v, got %v", callTracker, server.CallTracker)
+	}
+	if server.Version != version {
+		t.Errorf("Expected Version to be %s, got %s", version, server.Version)
+	}
+	if server.Commit != commit {
+		t.Errorf("Expected Commit to be %s, got %s", commit, server.Commit)
+	}
+	if server.Started {
+		t.Error("Expected Started to be false")
+	}
 }

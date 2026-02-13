6006084f74da17b6983eab6847b5dec255fc8451
Jacob McSwain <jacob@mcswain.dev>
2026-02-12 23:32:26 -0600

    fix license headers

--- internal/http/frontend/src/components/setup/HTTP/CORSSettings.vue
+++ internal/http/frontend/src/components/setup/HTTP/CORSSettings.vue
@@ -1,7 +1,7 @@
 <!--
   SPDX-License-Identifier: AGPL-3.0-or-later
   DMRHub - Run a DMR network server in a single binary
-  Copyright (C) 2023-2024 Jacob McSwain
+  Copyright (C) 2023-2026 Jacob McSwain
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU Affero General Public License as published by
@@ -14,7 +14,7 @@
   GNU Affero General Public License for more details.
 
   You should have received a copy of the GNU Affero General Public License
-  along with this program. If not, see <https:  www.gnu.org/licenses/>.
+  along with this program. If not, see <https://www.gnu.org/licenses/>.
 
   The source code is available at <https://github.com/USA-RedDragon/DMRHub>
 -->
